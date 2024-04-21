b052cf2482762b1003f3a58e555a517e3799a293
NafiulAntu <157912936+NafiulAntu@users.noreply.github.com>
2024-04-21 20:35:46 +0600

    Update rock.py

--- rock.py
+++ rock.py
@@ -1,7 +1,43 @@
 import random
 
+choice = ['rock', 'paper', 'scissors']
 
-myList = ["rock", "paper", "scissors"]
-z = random.choice(myList)
+computer = random.choice(choice)
+player = None
 
-print(z)
+while player not in choice:
+    player = input("rock,paper,scissors :").lower()
+
+if player == computer:
+    print("player: ", player)
+    print("computer: ", computer)
+    print("Tie!")
+
+elif player == "rock":
+    if computer == "paper":
+        print("player: ", player)
+        print("computer: ", computer)
+        print("You lose!")
+        if computer == "scissors":
+            print("player: ", player)
+            print("computer: ", computer)
+            print("You Win!")
+
+elif player == "scissors":
+    if computer == "rock":
+        print("player: ", player)
+        print("computer: ", computer)
+        print("You lose!")
+        if computer == "paper":
+            print("player: ", player)
+            print("computer: ", computer)
+            print("You Win!")
+elif player == "paper":
+    if computer == "scissors":
+        print("player: ", player)
+        print("computer: ", computer)
+        print("You lose!")
+        if computer == "rock":
+            print("player: ", player)
+            print("computer: ", computer)
+            print("You Win!")
